d407c906677a928aa7686aa04052f97cbd9dc3c3
HP <bilimus@gmail.com>
2021-04-01 00:18:36 +0500

    Initial commit

---
+++ src/test/java/com/qaforeveryone/spring_boot/thyleaf/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qaforeveryone.spring_boot.thyleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
